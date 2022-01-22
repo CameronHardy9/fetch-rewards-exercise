aa413a362af407c4ce2102839d2bedc3da43f824
Cameron Hardy <camhardy99@gmail.com>
2022-01-22 10:27:15 -0600

    Added logic for basic loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import {Routes, Route} from 'react-router-dom';
+import {useState, useEffect} from 'react';
 import Form from './components/Form';
 import Success from './components/Success';
 
 function App() {
+    const [loaded, setLoaded] = useState(false);
+
     return (
         <>
-            <Routes>
-                <Route path="/" element={<Form />} />
-                <Route path="/success" element={<Success />} />
-            </Routes>
+            {loaded ? (
+                <Routes>
+                    <Route path="/" element={<Form />} />
+                    <Route path="/success" element={<Success />} />
+                </Routes>
+            ) : (
+                <h1>Loading...</h1>
+            )}
+            
         </>
     );
 }
