cb52925630eb2b76ec5c5b2334d02feeb66b4a42
Cameron Hardy <camhardy99@gmail.com>
2022-01-22 10:54:36 -0600

    Added basic form framework and pulled field data props from parent

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,36 @@
-function Form() {
+function Form(props) {
     return(
         <>
-            <h1>Form</h1>
+            <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around'}}>
+                <label htmlFor='name'>Full Name</label>
+                <input type="text" name="name" id="name" />
+                <label htmlFor='email'>Email</label>
+                <input type="email" name="email" id="email" />
+                <label htmlFor='password'>Create Password</label>
+                <input type="password" name="password" id="password" />
+                <label htmlFor='confirmPassword'>Confirm Password</label>
+                <input type="password" name="confirmPassword" id="confirmPassword" />
+                <label htmlFor='occupation'>Occupation</label>
+                <select name="occupation" id="occupation">
+                    {
+                        props.occupations.map((item) => {
+                            return(
+                                <option value={item.toLowerCase().replace(' ', '-')}>{item}</option>
+                            )
+                        })
+                    }
+                </select>
+                <label htmlFor='state'>State</label>
+                <select name="state" id="state">
+                    {
+                        props.states.map((item) => {
+                            return(
+                                <option value={item.name.toLowerCase()}>{item.name}</option>
+                            )
+                        })
+                    }
+                </select>
+            </form>
         </>
     )
 };
