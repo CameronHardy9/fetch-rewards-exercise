14542ef2db6f8a9cbdac128b13ede85928b506f8
Cameron Hardy <camhardy99@gmail.com>
2022-01-22 10:29:01 -0600

    Added state and effect hooks for gathering field data and passing to child component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,31 @@ import {Routes, Route} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import Form from './components/Form';
 import Success from './components/Success';
+import apiHandler from './utils/apiHandler';
 
 function App() {
     const [loaded, setLoaded] = useState(false);
+    const [states, setStates] = useState(undefined);
+    const [occupations, setOccupations] = useState(undefined);
+
+    useEffect(() => {
+        console.log("effect");
+        (async () => {
+            const fieldData = await apiHandler("GET");
+
+            if (fieldData.occupations && fieldData.states) {
+                setStates(fieldData.states);
+                setOccupations(fieldData.occupations);
+                setLoaded(true);
+            }
+        })();
+    }, [])
 
     return (
         <>
             {loaded ? (
                 <Routes>
-                    <Route path="/" element={<Form />} />
+                    <Route path="/" element={<Form states={states} occupations={occupations} />} />
                     <Route path="/success" element={<Success />} />
                 </Routes>
             ) : (
