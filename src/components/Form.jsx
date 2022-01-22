1549ebbc0c102ebfb964170b3603d27291c76def
Cameron Hardy <camhardy99@gmail.com>
2022-01-22 11:27:34 -0600

    Added temp styling for better element alignment

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,35 +3,47 @@ import uniqid from 'uniqid';
 function Form(props) {
     return(
         <>
-            <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around'}}>
-                <label htmlFor='name'>Full Name</label>
-                <input type="text" name="name" id="name" />
-                <label htmlFor='email'>Email</label>
-                <input type="email" name="email" id="email" />
-                <label htmlFor='password'>Create Password</label>
-                <input type="password" name="password" id="password" />
-                <label htmlFor='confirmPassword'>Confirm Password</label>
-                <input type="password" name="confirmPassword" id="confirmPassword" />
-                <label htmlFor='occupation'>Occupation</label>
-                <select name="occupation" id="occupation">
-                    {
-                        props.occupations.map((item) => {
-                            return(
-                                <option key={uniqid()} value={item.toLowerCase().replace(' ', '-')}>{item}</option>
-                            )
-                        })
-                    }
-                </select>
-                <label htmlFor='state'>State</label>
-                <select name="state" id="state">
-                    {
-                        props.states.map((item) => {
-                            return(
-                                <option key={uniqid()} value={item.name.toLowerCase()}>{item.name}</option>
-                            )
-                        })
-                    }
-                </select>
+            <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', flexGrow: 1}}>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <label htmlFor='name'>Full Name</label>
+                    <input type="text" name="name" id="name" />
+                </div>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <label htmlFor='email'>Email</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <label htmlFor='password'>Create Password</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <label htmlFor='confirmPassword'>Confirm Password</label>
+                    <input type="password" name="confirmPassword" id="confirmPassword" />
+                </div>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <label htmlFor='occupation'>Occupation</label>
+                    <select name="occupation" id="occupation">
+                        {
+                            props.occupations.map((item) => {
+                                return(
+                                    <option key={uniqid()} value={item.toLowerCase().replace(' ', '-')}>{item}</option>
+                                )
+                            })
+                        }
+                    </select>
+                </div>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    <label htmlFor='state'>State</label>
+                    <select name="state" id="state">
+                        {
+                            props.states.map((item) => {
+                                return(
+                                    <option key={uniqid()} value={item.name.toLowerCase()}>{item.name}</option>
+                                )
+                            })
+                        }
+                    </select>
+                </div>
                 <button>Submit</button>
             </form>
         </>
