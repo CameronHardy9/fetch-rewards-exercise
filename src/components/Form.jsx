7ce6d5f4b59602d2a65aa6788f7d95f380335a36
Cameron Hardy <camhardy99@gmail.com>
2022-01-22 11:31:07 -0600

    Refactored styling for easier manipulation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,40 @@
 import uniqid from 'uniqid';
 
+const divStyling = {
+    display: 'flex', 
+    flexDirection: 'column', 
+    alignItems: 'center'
+};
+
+const formStyling = {
+    display: 'flex', 
+    flexDirection: 'column', 
+    alignItems: 'center', 
+    justifyContent: 'space-around', 
+    flexGrow: 1
+}
+
 function Form(props) {
     return(
         <>
-            <form style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around', flexGrow: 1}}>
-                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <form style={formStyling}>
+                <div style={divStyling}>
                     <label htmlFor='name'>Full Name</label>
                     <input type="text" name="name" id="name" />
                 </div>
-                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <div style={divStyling}>
                     <label htmlFor='email'>Email</label>
                     <input type="email" name="email" id="email" />
                 </div>
-                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <div style={divStyling}>
                     <label htmlFor='password'>Create Password</label>
                     <input type="password" name="password" id="password" />
                 </div>
-                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <div style={divStyling}>
                     <label htmlFor='confirmPassword'>Confirm Password</label>
                     <input type="password" name="confirmPassword" id="confirmPassword" />
                 </div>
-                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <div style={divStyling}>
                     <label htmlFor='occupation'>Occupation</label>
                     <select name="occupation" id="occupation">
                         {
@@ -32,7 +46,7 @@ function Form(props) {
                         }
                     </select>
                 </div>
-                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <div style={divStyling}>
                     <label htmlFor='state'>State</label>
                     <select name="state" id="state">
                         {
