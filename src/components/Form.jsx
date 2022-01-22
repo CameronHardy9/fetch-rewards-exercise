50cfcae8956a32f46fa4aacb9a780900cc5a628a
Cameron Hardy <camhardy99@gmail.com>
2022-01-22 11:50:09 -0600

    Added state for user inputs. Made fields required.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import uniqid from 'uniqid';
+import {useState} from 'react';
 
 const divStyling = {
     display: 'flex', 
@@ -15,28 +16,36 @@ const formStyling = {
 }
 
 function Form(props) {
+    const [formData, setFormData] = useState({
+        name: undefined,
+        email: undefined,
+        password: undefined,
+        occupation: undefined,
+        state: undefined
+    })
+
     return(
         <>
             <form style={formStyling}>
                 <div style={divStyling}>
                     <label htmlFor='name'>Full Name</label>
-                    <input type="text" name="name" id="name" />
+                    <input type="text" name="name" id="name" required />
                 </div>
                 <div style={divStyling}>
                     <label htmlFor='email'>Email</label>
-                    <input type="email" name="email" id="email" />
+                    <input type="email" name="email" id="email" required />
                 </div>
                 <div style={divStyling}>
                     <label htmlFor='password'>Create Password</label>
-                    <input type="password" name="password" id="password" />
+                    <input type="password" name="password" id="password" required />
                 </div>
                 <div style={divStyling}>
                     <label htmlFor='confirmPassword'>Confirm Password</label>
-                    <input type="password" name="confirmPassword" id="confirmPassword" />
+                    <input type="password" name="confirmPassword" id="confirmPassword" required />
                 </div>
                 <div style={divStyling}>
                     <label htmlFor='occupation'>Occupation</label>
-                    <select name="occupation" id="occupation">
+                    <select name="occupation" id="occupation" required>
                         {
                             props.occupations.map((item) => {
                                 return(
@@ -48,7 +57,7 @@ function Form(props) {
                 </div>
                 <div style={divStyling}>
                     <label htmlFor='state'>State</label>
-                    <select name="state" id="state">
+                    <select name="state" id="state" required>
                         {
                             props.states.map((item) => {
                                 return(
